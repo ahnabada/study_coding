255692ed0d25cce54317bd2107446a16dde32789
Haeungoo <43082157+Haeungoo@users.noreply.github.com>
2023-02-26 23:59:15 +0900

    Update and rename src/main/java/study/ahnabada/programmers/code/stack_queue/Lv2_truck_via_bridge.java to src/main/java/study/ahnabada/programmers/code/stack_queue/다리를_지나는_트럭/Solution_he.java
    refactor: 코드 리뷰를 위한 포맷 통일

--- src/main/java/study/ahnabada/programmers/code/stack_queue/다리를_지나는_트럭/Solution_he.java
+++ src/main/java/study/ahnabada/programmers/code/stack_queue/다리를_지나는_트럭/Solution_he.java
@@ -1,9 +1,9 @@
-package org.example.programmers.Lv2;
+package study.ahnabada.programmers.code.stack_queue.다리를_지나는_트럭;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Lv2_truck_via_bridge {
+public class Solution_he {
 
 
     public static void main(String[] args) {
